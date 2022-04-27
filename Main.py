649753a8732579e09da3ceeeb0a0764f8b00f701
NemoBitBot <84641717+NemoBitBot@users.noreply.github.com>
2022-04-27 16:47:19 -0300

    agrupamiento en funciones de la simulacion agendo tiempos:
    Ingrese la cantidad de horas:10000000 (10M)
    Ganancia promedio por hora de voluntariado: 123.743
    Ganancia promedio por hora del callcenter: 112.618
    Tiempo usado en la simulacion: 229.04s
    ----------------------------------------------------
    Ingrese la cantidad de horas:5000000 (5M)
    Ganancia promedio por hora de voluntariado: 123.728
    Ganancia promedio por hora del callcenter: 112.615
    Tiempo usado en la simulacion: 79.86s
    ----------------------------------------------------
    Ingrese la cantidad de horas:1000000 (1M)
    Ganancia promedio por hora de voluntariado: 123.763
    Ganancia promedio por hora del callcenter: 112.668
    Tiempo usado en la simulacion: 21.8s

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 import numpy as np
 import pandas as pd
@@ -6,6 +7,11 @@ import pandas as pd
 
 horas = int(input("Ingrese la cantidad de horas:"))
 cantidad = 20*horas
+
+
+def truncate(self, values, decs=0):
+    """funcion utilizada para truncar nuevaDistr y no trabajar con todos los decimales de python """
+    return np.trunc(values * 10 ** decs) / (10 ** decs)
 
 def situacion(rnd_persona):
     if rnd_persona < 0.15:
@@ -18,7 +24,6 @@ def situacion(rnd_persona):
         return True, "hombre"
     else:
         return False, "hombre"
-
 
 def gasto(sexo,rnd):
     if sexo == "hombre":
@@ -40,20 +45,30 @@ def gasto(sexo,rnd):
         else:
             return 25
 
-
-llamadasHora = [20,28]
-acum= [0,0]
-for i in range(len(llamadasHora)):
+def simular(horas, cantLlamadas):
+    acum = 0
     for j in range(horas):
-        for k in range(llamadasHora[i]):
+        for k in range(cantLlamadas):
             rnd1 = random.random()
 
             consume,sexo = situacion(rnd1)
             if consume:
                 rnd2 = random.random()
 
-                acum[i] += gasto(sexo,rnd2)
+                acum += gasto(sexo,rnd2)
 
-acum[1] *= 0.65
-acum = [acum[i]/horas for i in range(len(acum))]
-print(acum)
+    return acum / horas
+
+llamadasHora = [20,28]
+
+start = time.time()
+
+resultado1 = simular(horas, llamadasHora[0])
+resultado2 = simular(horas, llamadasHora[1]) * 0.65
+
+time = time.time()-start
+
+
+print("Ganancia promedio por hora de voluntariado: "+str(round(resultado1, 3))+"\n"
+      "Ganancia promedio por hora del callcenter: "+str(round(resultado2, 3)))
+print("Tiempo usado en la simulacion: "+str(round(time, 2))+"s")
