ef700208b7f78b00d58506429fa40c4f77935d97
Maru <83410136+Luuzaall@users.noreply.github.com>
2022-04-30 16:42:57 -0300

    Nuevo archivo de simulaciones.py

--- logicaPantallaIngresoDatos.py
+++ logicaPantallaIngresoDatos.py
@@ -1,6 +1,8 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow
 
+import simulaciones
+
 
 class PantallaIngresoDatos(QMainWindow):
     """Incializar clase"""
@@ -10,12 +12,12 @@ class PantallaIngresoDatos(QMainWindow):
 
         """Cargar la GUI"""
         uic.loadUi("defPantallaIngresoDatos.ui", self)
-        # self.btnSimular.clicked.connect(self.nuevaSimulacion)
+        self.btnSimular.clicked.connect(self.nuevaSimulacion)
 
     def cargarResultados(self, ganVoluntariado, ganCall, tiempoSim):
         self.txtGanVol.setText(ganVoluntariado)
         self.txtGanCall.setText(ganCall)
         self.txtTiempo.setText(str(tiempoSim))
 
-    # def nuevaSimulacion(self):
-    #     Main.nuevaSimulacion(int(self.txtCantHoras.text()), self)
+    def nuevaSimulacion(self):
+        simulaciones.nuevaSimulacion(int(self.txtCantHoras.text()), self)
